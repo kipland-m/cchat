46f35e964287409e11ba59f98c82dfa99f4482b4
Kipland Melton <kiplandmelton@gmail.com>
2024-07-06 00:27:05 -0500

    added if statement to handle if no port was given, creating variable to store information pertaining to connections

--- src/server.c
+++ src/server.c
@@ -14,6 +14,16 @@ void error(const char *msg){
 }
 
 int main(int argc, char *argv[]){
-    printf("test");
+	if (argc < 2){
+		fprintf(stderr, "please provide a port #\n");
+		exit(1);
+	}
+
+	int sockfd, newsockfd, portno;
+	char buffer[255];
+
+	struct sockaddr_in serv_addr, cli_addr;
+	printf("\ntest");
 }
+
 
